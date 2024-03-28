5c0200d471566d539ec27779901174bdc824cf0b
rb183899 <Tatyana.Sherstneva@rosbank.ru>
2024-03-29 00:50:16 +0300

    ссылка на файл с вопросами

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -18,6 +18,9 @@ const Dropdown = ({ text, children }) => {
       <div className={isOpen ? "content open" : "content"}>
         <article>{children}</article>
       </div>
+        <a className="faq-content-file-link" href="/download_file.html?file_id=7010832281905396898" download>
+            Памятка с ответами на частые вопросы
+        </a>
     </section>
   );
 };
