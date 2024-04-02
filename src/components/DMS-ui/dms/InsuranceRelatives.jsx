0ad7ffdff9d90faf56ff4a02a350a8bb2dff4828
Aleksandr Komarov <komar81@gmail.com>
2024-04-02 10:19:53 +0300

    up

--- src/components/DMS-ui/dms/InsuranceRelatives.jsx
+++ src/components/DMS-ui/dms/InsuranceRelatives.jsx
@@ -5,20 +5,19 @@ const InsuranceRelatives = () => {
     <Dropdown text={"Страхование родственников за счет собственных средств"}>
       <div className="insurance-relatives">
         <p>
-          Сотрудникам банка, застрахованным по программе ДМС, также
-          предоставляется услуга добровольного медицинского страхования
-          родственников (супруги, родители, дети), за счет собственных средств
-          застрахованного.
+          Сотрудникам банка, застрахованным по программе ДМС, также предоставляется услуга добровольного медицинского
+          страхования родственников (супруги, родители, дети), за счет собственных средств застрахованного.
         </p>
         <p>Услуга доступна в течение 90 дней с даты прикрепления к ДМС.</p>
 
-        <a
-          href="https://join.bestdoctor.ru/mnbvcxiusglaks"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {/* <a href="https://join.bestdoctor.ru/mnbvcxiusglaks" target="_blank" rel="noopener noreferrer"> */}
+        <a href="https://join.bestdoctor.ru/family_insurance_" target="_blank" rel="noopener noreferrer">
           Узнать подробнее
         </a>
+        <p>
+          * Если ссылка на сайт BestDoctor не открывается с рабочего компьютера, перешлите ее на личное устройство для
+          просмотра
+        </p>
       </div>
     </Dropdown>
   );
